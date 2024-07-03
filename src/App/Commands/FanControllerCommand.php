7edacf18ed5e038075e856af474274fee167b6a9
Armin Winkler <armin.winkler96@gmail.com>
2024-07-03 20:52:44 +0200

    feat: add telegram channel logging

--- src/App/Commands/FanControllerCommand.php
+++ src/App/Commands/FanControllerCommand.php
@@ -4,6 +4,8 @@ namespace Console\App\Commands;
 
 use Console\App\Services\Shelly\ShellyService;
 use Console\App\Services\SwitchBot\SwitchBotService;
+use Monolog\Handler\TelegramBotHandler;
+use Monolog\Logger;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -21,7 +23,11 @@ class FanControllerCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $logger = new Logger('log');
+        $logger->pushHandler(new TelegramBotHandler($_ENV['TELEGRAM_API_KEY'], $_ENV['TELEGRAM_CHANNEL']));
+
         $output->writeln("Starting process...");
+        $logger->info("Starting fan check.");
 
         $shelly = new ShellyService();
         $switchbot = new SwitchBotService();
@@ -36,11 +42,13 @@ class FanControllerCommand extends Command
                     $result = $shelly->plugTurnOff();
                     break;
                 case 'auto':
-                    $result = $switchbot->runByDewpoint(function () use ($shelly, $output) {
+                    $result = $switchbot->runByDewpoint(function () use ($shelly, $output, $logger) {
                         $output->writeln("Turning fan on.");
+                        $logger->info("Turing fan on");
                         $shelly->plugTurnOn();
-                    }, function () use ($shelly, $output) {
+                    }, function () use ($shelly, $output, $logger) {
                         $output->writeln("Turning fan off.");
+                        $logger->info("Turing fan off");
                         $shelly->plugTurnOff();
                     });
                     break;
@@ -51,15 +59,20 @@ class FanControllerCommand extends Command
         } catch (\Throwable $exception) {
             $output->writeln("Command failed.");
             $output->writeln($exception->getMessage());
+
+            $logger->error('Command failed.');
+            $logger->error($exception->getMessage());
+
             return Command::FAILURE;
         }
 
         if (!$result) {
+            $logger->info('Error was returned.');
             return Command::FAILURE;
         }
 
+        $logger->info('Job succeeded.');
         $output->writeln("Job succeeded.");
         return Command::SUCCESS;
-
     }
 }
